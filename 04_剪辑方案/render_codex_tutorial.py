214b72e489952a242d7296b16890153d00fc220f
Mr-Salticidae <wc1225827513@gmail.com>
2026-07-16 17:36:34 +0800

    修复:字幕在底栏内垂直居中

--- 04_剪辑方案/render_codex_tutorial.py
+++ 04_剪辑方案/render_codex_tutorial.py
@@ -73,7 +73,7 @@ ScaledBorderAndShadow: yes
 
 [V4+ Styles]
 Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
-Style: Default,Microsoft YaHei,40,&H00FFFFFF,&H00FFFFFF,&H00101010,&H80000000,-1,0,0,0,100,100,0,0,1,1.5,0,2,60,60,34,1
+Style: Default,Microsoft YaHei,40,&H00FFFFFF,&H00FFFFFF,&H00101010,&H80000000,-1,0,0,0,100,100,0,0,1,1.5,0,2,60,60,76,1
 
 [Events]
 Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
